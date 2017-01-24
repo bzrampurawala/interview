7cb6db82fe13e38535c42c6d1649083a6c59e0e0
burhanuddinrampurawala <burhanuddinzrampurawala@gamil.com>
2017-01-24 23:46:44 +0530

    random questions and respective choices

--- randomquestions.php
+++ randomquestions.php
@@ -4,20 +4,60 @@
          $conn = $db->connection();
          $array =  array('0' => 0);
          $question = array();
-         for ($i=1; $i <=5; $i++) { 
-            $num = rand( 1, 10 );
-            if (check($array, $num)) {
-               $i--;
-               continue;
-            } 
+         $sql = " Select uid From questions";
+         $result = mysqli_query($conn,$sql);
+         if(mysqli_num_rows($result) > 0 )
+         {
+           while ($row = mysqli_fetch_assoc($result)) 
+           {
+             $uid[] = $row["uid"];
+           }
+
+         }
+         for ($i=1; $i <=3; $i++) { 
+            $num = rand( 1, 15 );
+            if ($uid[$num-1] == 0) {
+               if (check($array, $num)) {
+                  $i--;
+                  continue;
+               } 
+               
+               $query = mysqli_query($conn,"SELECT * FROM questions WHERE id = '$num'");
+               $row = mysqli_fetch_assoc($query);
+               $question[$i] = $row["question"];
+               $array[$i] = $num;
+            }
             
-            $query = mysqli_query($conn,"SELECT * FROM qanda WHERE id = '$num'");
-            $row = mysqli_fetch_assoc($query);
-            $question[$i] = $row["question"];
-            check($array, $num);
-            $array[$i] = $num;
+         }
+         for ($i=4; $i <=5; $i++) { 
+            $num = rand( 16, 25 );
+            if ($uid[$num-1] == 1) {
+               if (check($array, $num)) {
+                  $i--;
+                  continue;
+               } 
+               
+               $query = mysqli_query($conn,"SELECT * FROM questions WHERE id = '$num'");
+               $row = mysqli_fetch_assoc($query);
+               $question[$i] = $row["question"];
+               $array[$i] = $num;
+            }
+            
          }
          echo json_encode($question);
+         $k = 1;
+         for ($j=1; $j <4 ; $j++) { 
+            $num = $array[$j]*4;
+            $limit = $num - 4;
+           while ($num>$limit) { 
+               $query = mysqli_query($conn,"SELECT * FROM answers WHERE id = '$num'");
+               $row = mysqli_fetch_assoc($query);
+               $answer[$k] = $row["answers"];
+               $num--;
+               $k++;
+          }
+         }
+         echo json_encode($answer);
          mysqli_close($conn);
          function check($array, $num)
          {
